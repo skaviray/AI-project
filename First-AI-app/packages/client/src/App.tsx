9826b430dd71fd52750438aedee331f6ce62a71c
Shiva Prasad Kavirayani <kavirayani1991@gmail.com>
2025-09-12 17:34:43 +0530

    Setup prettier

--- First-AI-app/packages/client/src/App.tsx
+++ First-AI-app/packages/client/src/App.tsx
@@ -1,20 +1,20 @@
-import { useState, React, useEffect } from 'react'
+import { useState, React, useEffect } from 'react';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
-
+import './App.css';
+import { Button } from './components/ui/button';
 
 export default function App() {
-  const [message, setMessage] = useState('')
-  useEffect(() => {
-    fetch("/api/info").then(res => res.json()).then(data => setMessage(data.message))
-  }, [])
-  return (
-    <>
-      <p className="font-bold p-20 text-5xl">{message}</p>
-      <Button>Click Me</Button>
-    </>
-  )
+   const [message, setMessage] = useState('');
+   useEffect(() => {
+      fetch('/api/info')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
+   return (
+      <>
+         <p className="font-bold p-20 text-5xl">{message}</p>
+         <Button>Click Me</Button>
+      </>
+   );
 }
-
